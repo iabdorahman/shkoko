4e99cdea75ab97631bb9fe45c0bb05f0c2adb484
Abdorahman <ssoda3@gmail.com>
2019-12-23 12:40:27 +0200

    npm run dev

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('mobile')->unique();
+            $table->string('email')->unique()->nullable();
+            $table->string('address');
+            $table->string('image')->default('user.png');
+            $table->string('active')->default('0');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
