8c6f1c49b2a8621128a2932dea818759b848386d
Abdorahman <ssoda3@gmail.com>
2019-12-23 13:01:59 +0200

    create migration files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique()->nullable();
             $table->string('address');
             $table->string('image')->default('user.png');
-            $table->string('active')->default('0');
+            $table->string('status')->default('0');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
